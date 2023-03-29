7d2d00cdc972177cb7689d175d30e0c2412d4878
{ZZZZZZZIWON} <{pjw20120436@gmail.com}>
2023-03-30 08:54:10 +0900

    0330 0853

--- Java_review/src/review_0328/RandomNumber10.java
+++ Java_review/src/review_0328/RandomNumber10.java
@@ -15,29 +15,28 @@ public class RandomNumber10 {
 				if (Random_num[i] == Random_num[j])
 					i--;
 		}
-			for(int num : Random_num)
+		for(int num : Random_num)
 			System.out.print(num+ " ");
 			System.out.println();
 			
-			//최댓값 구하기
-			for(int l = 0; l < 10; l++) 
-			{
 			int max = 0;
 			int max_index = 0;
-			for(int k= l+1 ; k < 10; k++) {
+			//최댓값 구하기
+		for(int l = 0; l < 10; l++) 
+			{
+			
+			for(int k= l + 1 ; k < 10; k++) {
 				if(max < Random_num[k])
 					max = Random_num[k];
 					max_index = k;
 			}
-			System.out.print("max = "+max);
-			System.out.println();
-			
-			int temp=0;
-			Random_num[l] = Random_num[max_index];
-			Random_num[max_index] = Random_num[l];
-			Random_num[l] = temp;
+		int temp=0;
+		Random_num[l] = Random_num[max_index];
+		Random_num[max_index] = Random_num[l];
+		Random_num[l] = temp;
 			}
-			
+		System.out.print("max = "+max);
+		System.out.println();
 			
 	}
 
