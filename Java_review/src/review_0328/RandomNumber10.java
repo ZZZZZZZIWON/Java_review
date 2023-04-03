6721681d771ead10c853235b408e343d42da5faf
{ZZZZZZZIWON} <{pjw20120436@gmail.com}>
2023-04-03 10:06:45 +0900

    0403 1006

--- Java_review/src/review_0328/RandomNumber10.java
+++ Java_review/src/review_0328/RandomNumber10.java
@@ -7,58 +7,58 @@ public class RandomNumber10 {
 
 	public static void main(String[] args) {
 
-		int Random_num[] = new int[10];
-
-		for (int i = 0; i < 10; i++) {
-			Random_num[i] = 1 + (int) (Math.random() * 10);
-			for (int j = 0; j < i; j++)
-				if (Random_num[i] == Random_num[j])
-					i--;
-		}
-		for(int num : Random_num)
-			System.out.print(num+ " ");
-			System.out.println();
-			
-			
-			//ÃÖ´ñ°ª ±¸ÇÏ±â
-			int max = 0;
-			int max_index = 0;
-			//ÃÖ´ñ°ª ±¸ÇÏ±â
-		for(int l = 0; l < 10; l++) 
-			{
-			
-			for(int k= l + 1 ; k < 10; k++) {
-			
-				for(int l = 0; l < 10; l++) 
-				{
-			
-				for(int k= l+1 ; k < 10; k++) {
-				if(max < Random_num[k])
-					max = Random_num[k];
-					max_index = k;
-			}
-		int temp=0;
-		Random_num[l] = Random_num[max_index];
-		Random_num[max_index] = Random_num[l];
-		Random_num[l] = temp;
-			}
-		System.out.print("max = "+max);
-		System.out.println();
-			
-				int temp=0;
-				Random_num[l] = Random_num[max_index];
-				Random_num[max_index] = Random_num[l];
-				Random_num[l] = temp;
-			}
-			
-			System.out.print("max = "+max);
-			System.out.println();
-			
-			for(int num : Random_num)
-				System.out.print(num+ " ");
-				System.out.println();
-			
-			}
+//		int Random_num[] = new int[10];
+//
+//		for (int i = 0; i < 10; i++) {
+//			Random_num[i] = 1 + (int) (Math.random() * 10);
+//			for (int j = 0; j < i; j++)
+//				if (Random_num[i] == Random_num[j])
+//					i--;
+//		}
+//		for(int num : Random_num)
+//			System.out.print(num+ " ");
+//			System.out.println();
+//			
+//			
+//			//ÃÖ´ñ°ª ±¸ÇÏ±â
+//			int max = 0;
+//			int max_index = 0;
+//			//ÃÖ´ñ°ª ±¸ÇÏ±â
+//		for(int l = 0; l < 10; l++) 
+//			{
+//			
+//			for(int k= l + 1 ; k < 10; k++) {
+//			
+//				for(int l = 0; l < 10; l++) 
+//				{
+//			
+//				for(int k= l+1 ; k < 10; k++) {
+//				if(max < Random_num[k])
+//					max = Random_num[k];
+//					max_index = k;
+//			}
+//		int temp=0;
+//		Random_num[l] = Random_num[max_index];
+//		Random_num[max_index] = Random_num[l];
+//		Random_num[l] = temp;
+//			}
+//		System.out.print("max = "+max);
+//		System.out.println();
+//			
+//				int temp=0;
+//				Random_num[l] = Random_num[max_index];
+//				Random_num[max_index] = Random_num[l];
+//				Random_num[l] = temp;
+//			}
+//			
+//			System.out.print("max = "+max);
+//			System.out.println();
+//			
+//			for(int num : Random_num)
+//				System.out.print(num+ " ");
+//				System.out.println();
+//			
+//			}
 	}
 }
 	
