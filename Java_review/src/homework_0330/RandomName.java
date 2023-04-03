848b79ee10ebf14d12ddadca7debb75023a900f2
박지원 <pjw20120436@gmail.com>
2023-04-03 20:39:57 +0900

    0403 20:39

---
+++ Java_review/src/homework_0330/RandomName.java
@@ -0,0 +1,22 @@
+package homework_0330;
+
+import java.util.Scanner;
+
+public class RandomName {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		String[] Names = { "김여원", "김정현", "김준성", "김초봄", "박규리", "박민규", "박슬기", "박주연", "박지원", "심소영", "오연경", "윤진수", "이성진",
+				"이유진", "이태형", "전병남", "정성호", "정승복", "정웅기", "조아름", "최지현" };
+
+		System.out.print("몇 명을 뽑을까요? >> ");
+		int numOfPeople = scan.nextInt();
+
+		for (int i = 0; i < numOfPeople; i++) {
+			int selected = 1 + (int) (Math.random() * 21); 
+			System.out.println(Names[selected] + " "); 
+		}
+
+	}
+}
